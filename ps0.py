3609cf22f084f6f6bfdf33d15e13494c0feb17d2
FD3SA <farsh64@gmail.com>
2011-03-16 18:03:07 -0400

    update

---
+++ ps0.py
@@ -0,0 +1,10 @@
+print ("Enter your last name:")
+Last = raw_input()
+
+print ("Enter your first name:")
+First = raw_input()
+
+print First
+print Last
+
+print ("Good afternoon, Mr. " +Last+ ", I have been expecting you.")
